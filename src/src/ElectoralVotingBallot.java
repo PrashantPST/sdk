eaff2f94e1f47a8d8bb526e05b51a746797afc9c
prashant.kumar <prashant.kumar@milvik.se>
2019-06-27 15:19:24 +0530

    subarray sum

---
+++ src/src/ElectoralVotingBallot.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class ElectoralVotingBallot {
+    private static void findWinner(String[] votes) {
+        Map<String,Integer> map = new HashMap<>();
+        for (String str : votes) {
+            if (map.keySet().contains(str))
+                map.put(str, map.get(str) + 1);
+            else
+                map.put(str, 1);
+        }
+        int maxValueInMap = 0;
+        String winner = "";
+        for (Map.Entry<String,Integer> entry : map.entrySet()) {
+            String key = entry.getKey();
+            Integer val = entry.getValue();
+            if (val > maxValueInMap) {
+                maxValueInMap = val;
+                winner = key;
+            }
+            else if (val == maxValueInMap && winner.compareTo(key) > 0)
+                winner = key;
+        }
+        System.out.println(winner);
+    }
+    public static void main(String[] args)
+    {
+        String[] votes = { "john", "johnny", "jackie",
+                "johnny", "john", "jackie",
+                "jamie", "jamie", "john",
+                "johnny", "jamie", "johnny",
+                "john" };
+
+        findWinner(votes);
+    }
+}
---
+++ src/src/FindAllSubarrayWithGivenSum.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class FindAllSubarrayWithGivenSum {
+    private static <K,V> void insert(Map<K, List<V>> hashMap, K key, V value) {
+        if (!hashMap.containsKey(key)) {
+            hashMap.put(key, new ArrayList<>());
+        }
+        hashMap.get(key).add(value);
+    }
+    private static void printSubarray(int[] A, int i, int j)
+    {
+        System.out.println(Arrays.toString(Arrays.copyOfRange(A, i, j + 1)));
+    }
+    private static void printallSubarrays(int[] A, int sum) {
+        Map<Integer, List<Integer>> hashMap = new HashMap<>();
+        insert(hashMap, 0, -1);
+
+        int sum_so_far = 0;
+        for (int index = 0; index < A.length; index++) {
+            sum_so_far += A[index];
+            if (hashMap.containsKey(sum_so_far - sum)) {
+                List<Integer> list = hashMap.get(sum_so_far - sum);
+                for (Integer value: list) {
+                    printSubarray(A, value + 1, index);
+                }
+            }
+            insert(hashMap, sum_so_far, index);
+        }
+    }
+    public static void main (String[] args) {
+        int[] A = { 3, 4, -7, 1, 3, 3, 1, -4 };
+        int sum = 7;
+        printallSubarrays(A, sum);
+    }
+}
