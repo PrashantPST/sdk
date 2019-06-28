fa3b1904b3a62585647b3d01869d7ebca55c6ce4
prashant.kumar <prashant.kumar@milvik.se>
2019-06-29 04:43:53 +0530

    optimize version of nPr and nCr

---
+++ src/src/nprAndncr.java
@@ -0,0 +1,32 @@
+public class nprAndncr {
+    public static void main(String[] args) {
+        int n = 5;
+        int r = 2;
+        System.out.println(n+"P"+r+" = "+nPr(n, r));
+        System.out.println(n+"C"+r+" = "+nCr(n, r));
+    }
+
+    /*
+     * The value of C(n, r) can be calculated in O(r) time and O(1) extra space.
+     */
+    private static long nCr(int n, int r) {
+        int res = 1;
+        if (r > n - r)
+            r = n - r;
+        for (int i = 0; i < r; ++i) {
+            res *= (n - i);
+            res /= (i + 1);
+        }
+        return res;
+    }
+
+    /*
+     * The value of P(n, r) calculated in O(r) time and O(1) extra space.
+     */
+    private static long nPr(int n, int r) {
+        long res = 1;
+        while(r-- > 0)
+            res *= n--;
+        return res;
+    }
+}
