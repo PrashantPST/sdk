7c6523da995cf67de3d1ad96164199cdac0fb151
prashant.kumar <prashant.kumar@milvik.se>
2019-10-17 09:47:00 +0530

    get products of all numbers except at that index

--- src/ds/linear/arrays/Array.java
+++ src/ds/linear/arrays/Array.java
@@ -19,6 +19,36 @@ public class Array {
         System.out.println(Arrays.toString(findTwoUniqueNumbers(arr)));
     }
 
+    public static int[] getProductsOfAllIntsExceptAtIndex(int[] intArray) {
+
+        if (intArray.length < 2) {
+            throw new IllegalArgumentException("Getting the product of numbers at other indices requires at least 2 numbers");
+        }
+
+        // we make an array with the length of the input array to
+        // hold our products
+        int[] productsOfAllIntsExceptAtIndex = new int[intArray.length];
+
+        // for each integer, we find the product of all the integers
+        // before it, storing the total product so far each time
+        int productSoFar = 1;
+        for (int i = 0; i < intArray.length; i++) {
+            productsOfAllIntsExceptAtIndex[i] = productSoFar;
+            productSoFar *= intArray[i];
+        }
+
+        // for each integer, we find the product of all the integers
+        // after it. since each index in products already has the
+        // product of all the integers before it, now we're storing
+        // the total product of all other integers
+        productSoFar = 1;
+        for (int i = intArray.length - 1; i >= 0; i--) {
+            productsOfAllIntsExceptAtIndex[i] *= productSoFar;
+            productSoFar *= intArray[i];
+        }
+        return productsOfAllIntsExceptAtIndex;
+    }
+
 
     /**
      * Given an array in which all numbers except two are repeated once. (i.e. we have 2n+2 numbers and n numbers are occurring
